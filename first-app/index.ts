b597efcf45be7e5c8901dcd3f8045fe2bd6f795d
Shaw Lu <shawlu95@gmail.com>
2022-05-16 23:32:10 -0700

    Add interface typecheck

--- first-app/index.ts
+++ first-app/index.ts
@@ -2,4 +2,15 @@ import axios from 'axios';
 
 const url = 'https://jsonplaceholder.typicode.com/todos/1';
 
-axios.get(url).then(res => console.log(res.data));
+// define structure of object
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+};
+
+axios.get(url).then(res => {
+  const todo = res.data as Todo;
+  const { id, title, completed } = todo;
+  console.log(`The todo with ID ${id}: ${title} is ${completed}`);
+});
